8152f4837a994918eb67679c4eef3cb465d10c14
a.bmostafa@dev.thiqah.sa <a.bmostafa@dev.thiqah.sa>
2022-04-22 19:18:55 +0200

    demo

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,37 @@
+import { AfterContentChecked, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit, AfterContentChecked { 
+
+  products: any[] = [];
+  
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://fakestoreapi.com/products').subscribe((res) => {
+      (res as Array<any>).forEach(product => {
+        let {image, price, title} = product;
+        price = Number(price).toFixed(0);
+        this.products.push({image, price , title});
+      });
+    },
+    error=> {
+    });
+  }
+
+  ngAfterContentChecked(): void {
+    console.log([...document.getElementsByClassName('cards__item')]);
+    [...document.getElementsByClassName('cards__item')]?.forEach((el: Element)=> {
+      el.addEventListener('click', ()=> {
+        el.getElementsByClassName('cards__backoptions')[0]?.classList.toggle('v-none');
+      });
+    });  
+  }
+}
